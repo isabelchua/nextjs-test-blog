fd930abd0064330b465898b8a74e31c058ddc89e
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-18 11:45:43 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import fs from "fs";
+
+export default function Home({ slugs }) {
+	return (
+		<div>
+			slugs:
+			{slugs.map(slug => {
+				return (
+					<div key={slug}>
+						<Link href={"/blog/" + slug}>
+							<a>{"/blog/" + slug}</a>
+						</Link>
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
+export const getStaticProps = async () => {
+	const files = fs.readdirSync("posts");
+
+	return {
+		props: {
+			slugs: files.map(filename => filename.replace(".md", ""))
+		}
+	};
+};
